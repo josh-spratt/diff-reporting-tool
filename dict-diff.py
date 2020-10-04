9dad3301432328480fa055da291e9166e947a30a
josh-spratt <61611179+josh-spratt@users.noreply.github.com>
2020-10-04 12:16:37 -0500

    minor tweaks to deepdiff print

--- dict-diff.py
+++ dict-diff.py
@@ -4,13 +4,13 @@ import json
 
 
 def take_csv_and_create_dict(csv_path):
-	new_dict = {}
-	with open(csv_path, mode='r') as f:
-		reader = csv.DictReader(f)
-		for row in reader:
-			key = row['first']+'-'+row['last']
-			new_dict[key] = row
-	return new_dict		
+    new_dict = {}
+    with open(csv_path, mode='r') as f:
+        reader = csv.DictReader(f)
+        for row in reader:
+            key = row['first']+'-'+row['last']
+            new_dict[key] = row
+    return new_dict
 
 
 x = take_csv_and_create_dict('csv-1.csv')
@@ -18,25 +18,9 @@ y = take_csv_and_create_dict('csv-2.csv')
 
 
 def diff_new_dicts(old_dict, new_dict):
-	diff = DeepDiff(old_dict, new_dict)
-	return diff
+    diff = DeepDiff(old_dict, new_dict)
+    return diff
 
 
-z = diff_new_dicts(x, y).to_dict()
+z = diff_new_dicts(x, y).pretty()
 print(z)
-
-# print(json.dumps(z, indent=4))
-# with open('csv-1.csv', mode='r') as f:
-#     reader = csv.DictReader(f)
-#     for row in reader:
-#         key = row['first']+'-'+row['last']
-#         new_dict1[key] = row
-# print(json.dumps(new_dict1, indent=4))
-
-# with open('csv-2.csv', mode='r') as g:
-#     reader = csv.DictReader(g)
-#     for row in reader:
-        
-
-# x = DeepDiff(new_list2, new_list1)
-# print(json.dumps(x, indent=4))
